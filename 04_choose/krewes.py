85075bebe70b4b700577aa15b66c456e7017f308
Prattay <pdey30@stuy.edu>
2022-09-23 13:03:18 -0400

    upd

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -21,21 +21,19 @@ OPS SUMMARY:
 
 import random as rng
 
-krewes = {2:["a","b","c"], 7:["d","e","f"], 8:["g","h","i"]}
+krewes = {
+           2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY", "Ruiwen"], 
+           7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
+           8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "Wanying", "Kevin"]
+         }
+
 
 def generateRandomDev():
-    selected = rng.randint(1,3)
-    randPD = 0
-    if selected == 1:
-        randPD = 2
-    elif selected == 2:
-        randPD = 7
-    else:
-        randPD = 8 
-    selectedClass = randPD
-    fullClass = krewes[selectedClass]
-    selectedDevo = rng.randint(0, len(fullClass)-1)
-    print(selectedClass)
-    return fullClass[selectedDevo] 
+    keys = list(krewes.keys())
+    randIndex = rng.randint(0, len(krewes) - 1)
+    randKey = keys[randIndex]
+    fullPeriod = krewes[randKey]
+    selectedDevo = rng.randint(0, len(fullPeriod)-1)
+    return fullPeriod[selectedDevo]
 
-print("Randomized devo " + generateRandomDev())
+print( generateRandomDev() )
