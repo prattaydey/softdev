375fb4842be84dd703e54b2ead942e981bc03267
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2022-09-22 23:57:17 -0400

    Update krewes.py

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -1,23 +1,48 @@
 '''
+Jing Feng, Anna Fang, Prattay Dey
+SoftDev
+04_choose
+2022-09-22
+time spent: 
 DISCO:
 - You can have multiple key-value pairs for an index
-- You can access a specific value by entering its key after the index
--
+- You can access a specific value by entering its index after the key 
+- Using randint will return integers while simply using random will return floats 
 QCC:
-
 OPS SUMMARY:
 - We're thinking of importing the random python library for our RNG
-- We're thinking of using randint(a,b) twice.
+- We're thinking of using randint(a,b) twice. 
     - The first time we'll use it to find a random key
     - The 2nd time we'll find a random value within that key
+- We will have two methods
+    - The first method will use random to generate the class (2/7/8) that the random dev will be selected from
+    - The next will use random to provide the index of a randomly selected dev inside of the selected class
 '''
-import random
+
+import random as rng
 
 krewes = {2:["a","b","c"], 7:["d","e","f"], 8:["g","h","i"]}
 
-def krewes(devdict):
-    return devdict[0]
+# first, use random to randomly choose the class the random devo will be chosen from 
+def generateRandomClass():
+    selected = rng.randint(1,3)
+    randClass = 0
+    if selected == 1:
+        randClass = 2
+    elif selected == 2:
+        randClass = 7
+    else:
+        randClass = 8 
+    return randClass
 
-print(krewes[0])
-
-# print(krewes(krewes))
+# using the genenerated class, find the length of that list and randomly select from it. Then, return the chosen devo! 
+def generateRandomDev():
+    selectedClass = generateRandomClass()
+    fullClass = krewes[selectedClass]
+    selectedDevo = rng.randint(0, len(fullClass)-1)
+    #print(selectedClass)
+    #print(selectedDevo) 
+    return fullClass[selectedDevo] 
+    
+print(generateRandomDev())
+ 
