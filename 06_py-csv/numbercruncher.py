c9eeabacb727ee3d4fe0cccadd8eb3ac220d2094
Prattay <Prattay@Prashuns-iMac.local>
2022-09-29 23:26:19 -0400

    started 06

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -6,7 +6,9 @@ SoftDev
 time spent: _______
 
 DISCO:
-
+- When reading csv files, we can use DictReader() instead of reader(). This function is like a regular reader but also maps each row to a key-value pair.
+  (https://docs.python.org/3/library/csv.html)
+-
 QCC:
 
 HOW THIS SCRIPT WORKS:
@@ -17,6 +19,5 @@ import csv
 
 with open("occupations.csv", "r") as file:
     reader = csv.DictReader(file)
-
     for row in reader:
-        print(row["Job Class"], row["Percentage"])
+        print(row["Job Class"], row["Percentage"]) 
