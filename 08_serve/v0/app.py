541d1a3662f0523d606ba5c08929f536a72cf544
Prattay <Prattay@Prashuns-iMac.local>
2022-10-05 23:12:14 -0400

    upd

--- 08_serve/v0/app.py
+++ 08_serve/v0/app.py
@@ -1,14 +1,16 @@
-# Clyde 'Thluffy' Sinclair
+# Whole Lotta Coding: Prattay, Aden, Emerson
 # SoftDev
 # Oct 2022
 
+# Prediction: Will print 'No hablo queso!' at the root of the webpage
+# Outcome: Prediction was true
+
 from flask import Flask
-app = Flask(__name__) # ...
+app = Flask(__name__) # __name__ set to current module that is being run. Allows Flask to locate module when connecting server and browser
 
-@app.route("/") # ...
+@app.route("/") #  '/' points to the root, routing the function to the root URL.
 def hello_world():
-    print(__name__) # ...
-    return "No hablo queso!"  # ...
+    print(__name__) #
+    return "No hablo queso!"  #
 
-app.run()  # ...
-                
+app.run()  # runs the module
--- 08_serve/v1/app.py
+++ 08_serve/v1/app.py
@@ -2,6 +2,9 @@
 # SoftDev
 # Oct 2022
 
+# Prediction: Will show a blank webpage, as we have not printed __name__ first
+# Outcome: It still prints the return line, even without printing __name__ first. 
+
 from flask import Flask
 app = Flask(__name__) #create instance of class Flask
 
@@ -10,4 +13,3 @@ def hello_world():
     return "No hablo queso!"
 
 app.run()
-
