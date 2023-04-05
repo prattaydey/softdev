dcc2675b8cf33dd81fb99d44749a45e09c4511c4
Prattay <Prattay@Prashuns-iMac.local>
2023-04-04 22:23:42 -0400

    upd

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -1,8 +1,7 @@
-// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
-// SoftDev pd0
+// Team Peanut Butter :: Prattay Dey, Brian Yang
+// SoftDev pd07
 // K27 -- Basic functions in JavaScript
-// 2023-04-04t
-// --------------------------------------------------
+// 2023-04-03t
 
 
 // as a duo...
@@ -11,3 +10,25 @@
 
 
 //Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
+
+function fact(n){
+    if (n < 2){
+        return 1;
+    }
+    else { 
+        return n * fact(n-1); 
+    }
+}
+
+console.log(fact(5)); // should be 120
+
+function fib(n){
+    if (n < 2){
+        return n;
+    }
+    else {
+        return fib(n-2) + fib(n-1);
+    }
+}
+
+console.log(fib(5));  // should be 5
