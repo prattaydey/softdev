91cd10f4629284d46a9b68bc24b9548921b04735
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2023-04-24 23:45:01 -0400

    Update clickdraw.js

--- 30_jsdraw/clickdraw.js
+++ 30_jsdraw/clickdraw.js
@@ -13,27 +13,33 @@ var ctx = c.getContext("2d");
 var mode = "rect";
 
 // var toggleMode = function(e) {
-// var toggleMode = (e) => {
-//     console.log("toggling...");
-//     if () {
+ var toggleMode = (e) => {
+     console.log("toggling...");
+     if (mode == "rect") {
+       mode = "circle"
+       bToggler.innerHTML = "circle"
+     }
+     else {
+       mode = "rect"
+       bToggler.innerHTML = "rect"
 
-//     }
-//     else {
-
-//     }
-// }
+     }
+ }
 
 var drawRect = (e) => {
     var mouseX = e.offsetX;
     var mouseY = e.offsetY;
     console.log("mouseclick registered at ", mouseX, mouseY);
-
+    ctx.fillRect(mouseX, mouseY, 100,200);
 }
 
 var drawCircle = (e) => {
     var mouseX = e.offsetX;
     var mouseY = e.offsetY;
     console.log("mouseclick registered at ", mouseX, mouseY);
+    ctx.beginPath();
+    ctx.arc(mouseX, mouseY, 50, 0, 2 * Math.PI);
+    ctx.fill();
 }
 
 // checks if mode is rect or circle, then calls the respective draw function
@@ -42,17 +48,16 @@ var draw = (e) => {
         drawRect(e);
     }
     else {
-        drawCircle(e);      
+        drawCircle(e);
     }
 }
 
 var wipeCanvas = () => {
-    
+    ctx.clearRect(0, 0, c.width, c.height);
 }
 
 c.addEventListener("click", draw);
-// var bToggler = document. ;
-// bToggler. ;
-// var clearB = ;
-// clearB. ;
-
+var bToggler = document.getElementById("buttonToggle");
+bToggler.addEventListener("click", toggleMode);
+var clearB = document.getElementById("buttonClear");
+clearB.addEventListener("click", wipeCanvas);
