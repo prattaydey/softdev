70772b8753d14cf978013350caad9f685d6ab80e
Prattay <pdey30@stuy.edu>
2023-04-27 12:55:29 -0400

    fixed

--- 32_jsanim/animate.js
+++ 32_jsanim/animate.js
@@ -1,7 +1,7 @@
-// Team PreView :: Prattay Dey, Verit Li
+// Team PeanutButter: Prattay Dey, Brian Yang
 // SoftDev pd07
-// K32 -- canvas based JS animation
-// 2023-04-25t
+// K32 -- JSanim
+// 2023-04-26
 
 var c = document.getElementById("playground");
 var dotButton = document.getElementById("buttonCircle");
@@ -17,7 +17,8 @@ ctx.fillStyle = "#00ffff"; // cyan
 var requestID;
 
 var clear = (e) => {
-    e.preventDefault(); // 
+    //e.preventDefault();
+    //IDK WHAT THIS IS
     ctx.clearRect(0, 0, e.width, e.height);
 }
 
@@ -25,57 +26,65 @@ var radius = 0;
 var growing = true;
 
 var drawDot = () => {
-    // canceling frame prevents speeding up
-    window.cancelAnimationFrame( requestID );
-    requestID = window.requestAnimationFrame(drawDot);
-    // wipes canvas
-    clear(c);
-    //repaints circle
-    ctx.beginPath();
-    ctx.arc(c.width/2, c.height/2, radius, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fill();
+  // canceling frame prevents speeding up
+  window.cancelAnimationFrame( requestID );
+  requestID = window.requestAnimationFrame(drawDot);
+  // wipes canvas
+  clear(c);
+  //repaints circle
+  ctx.beginPath();
+  ctx.arc(c.width/2, c.height/2, radius, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
 
-    if (growing){
-      if (radius > c.width/2){
-        growing = false;
-      }
-      radius += 1;
+  if (growing){
+    if (radius > c.width/2){
+      growing = false;
+    }
+    radius += 1;
+  }
+  else{
+    if (radius == 0){
+      growing = true;
     }
     else{
-      if (radius == 0){
-        growing = true;
-      }
-      else{
-        radius -= 1;
-      }
+      radius -= 1;
     }
-     
+  }
+   
 }
 
 var dvdLogoSetup = () => {
-    window.cancelAnimationFrame( requestID );
+  window.cancelAnimationFrame( requestID );
+  clear(ctx);
 
-    var rectWidth = 60;
-    var rectHeight = 40;
+  var rectWidth = 60;
+  var rectHeight = 40;
 
-    var RectX = Math.floor( Math.random() * 380);
-    var RectY = Math.floor( Math.random() * 420);
+  // (x,y) upper left corner of image
+  var rectX = Math.floor( Math.random() * 440);
+  var rectY = Math.floor( Math.random() * 460);
 
-    var xVel = 2;
-    var yVel = 2;
+  var xVel = -1;
+  var yVel = -1;
 
-    var logo = new Image();
-    logo.src = "logo_dvd.jpg";
+  var logo = new Image();
+  logo.src = "logo_dvd.jpg";
 
-    var dvdLogo = () => {
-        ctx.clearRect(0, 0, c.width, c.height);
-        ctx.drawImage(rectX, rectY, rectWidth, rectHeight);
-        if ( rectX == 0 || rectX == 440){
-            
-        }
+  var dvdLogo = () => {
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.drawImage(logo, rectX, rectY, rectWidth, rectHeight);
+    if (rectX == 0 || rectX == 440) {
+      xVel = xVel * -1;
     }
-
+    if (rectY == 0 || rectY == 460) {
+      yVel = yVel * -1;
+    }
+    rectX += xVel;
+    rectY += yVel;
+    requestID = window.requestAnimationFrame(dvdLogo);
+  }
+  dvdLogo();
 }
 
 var stopIt = () => {
@@ -86,4 +95,5 @@ var stopIt = () => {
 }
 
 dotButton.addEventListener("click", drawDot);
+dvdButton.addEventListener("click", dvdLogoSetup)
 stopButton.addEventListener("click", stopIt);
