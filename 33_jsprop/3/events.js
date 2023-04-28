debfe066a085285a9ae0be3d2ad049c99498ae3d
Prattay <Prattay@Prashuns-iMac.local>
2023-04-27 23:38:06 -0400

    upd

--- 33_jsprop/3/events.js
+++ 33_jsprop/3/events.js
@@ -9,7 +9,7 @@ var clicky = function(e) {
   alert( this.innerHTML );
   //Q: What will happen when next line is uncommented?
   // We think that it will stop the additional pop-ups from appearing, so only the cell inner HTML will appear
-  // It stopped after only one, but it was the entire table inner html
+  // We were right in that it stopped after only one, but it was the entire table inner html not the cell. This is because of event capturing is set to true in line 31.
   e.stopPropagation();
 };
 
@@ -25,7 +25,9 @@ for (x=0; x < trs.length; x++) {
 //Q: What effect does the boolean arg have?
 //   (Leave exactly 1 version uncommented to test...)
 
-table.addEventListener('click', clicky, true);
-//table.addEventListener('click', clicky, false);
+// The boolean arg represents useCapture, which determines the order in which events are sent to the listener. If set to true, events highest in the DOM tree will be sent to the listener first. Otherwise, if false it will be events lower in the tree.
+
+// table.addEventListener('click', clicky, true);
+table.addEventListener('click', clicky, false);
 
 // Q: When user clicks on a cell, in what order will the pop-ups appear?
--- 33_jsprop/4/events.js
+++ 33_jsprop/4/events.js
@@ -10,25 +10,33 @@ var table = document.getElementsByTagName('table')[0];
 var clicky = function(e) {
   alert( this.innerHTML );
   //Q: What will happen when next line is uncommented?
-  //e.stopPropagation();
+  //Since the useCapture param is set to true for all the event listeners, only the highest one on the DOM tree (table) will show?
+  // e.stopPropagation();
+  //Prediction was right
 };
 
 
 //Q: Does the order in which the event listeners are attached matter?
+// No, even if you move around the event listeners it will still be the same sequence of pop ups
 
 //Predict, then test...
 //Q: What effect does the boolean arg have in each?
 //   (Leave exactly 1 version uncommented to test...)
+// - No changes seen for table data cell, since there is no event lower than it on the DOM tree
+// - For table row, if useCapture is false then cell would display before row. This is because cell is lower than row on the DOM tree.
+// - For table, if false then it would go down the DOM tree until it either:
+//   a) encounters an event with useCapture set to true or,
+//   b) reaches the event at the bottom of the DOM tree
 
 for (var x=0; x < tds.length; x++) {
   tds[x].addEventListener('click', clicky, true);
-  //tds[x].addEventListener('click', clicky, false);
+  // tds[x].addEventListener('click', clicky, false);
 }
 
 for (x=0; x < trs.length; x++) {
   trs[x].addEventListener('click', clicky, true);
-  //trs[x].addEventListener('click', clicky, false);
+  // trs[x].addEventListener('click', clicky, false);
 }
 
-table.addEventListener('click', clicky, true);
-//table.addEventListener('click', clicky, false);
+// table.addEventListener('click', clicky, true);
+table.addEventListener('click', clicky, false);
